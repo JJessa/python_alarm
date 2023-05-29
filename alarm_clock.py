06e19f35725d4088a5e08e14b3bbcf9c0160a25e
JJessa <jessicamosteiro13@gmail.com>
2023-05-29 10:20:31 +0200

    proyecto alarm-clock

---
+++ alarm_clock.py
@@ -0,0 +1,34 @@
+from playsound import playsound
+import time
+
+#This gives instructions to clean the terminal
+
+CLEAR = "\033[2J"
+
+#This gives instructions for the cursor to return to the beginning
+CLEAR_AND_RETURN = "\033[H"
+
+
+def alarm(seconds):
+    time_elapsed = 0
+    
+    print(CLEAR)
+    
+    while time_elapsed < seconds:
+        time.sleep(1) #Wait for a second before continuing so that the alarm doesn't go off immediately
+        time_elapsed += 1
+        
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+        
+        print(f"{CLEAR_AND_RETURN}La alarma sonará en: {minutes_left:02d}:{seconds_left:02d}")
+    
+    playsound("my_alarm.mp3")
+    
+minutes = int(input("Cuántos minutos para que suene la alarma: "))
+seconds = int(input("Cuántos segundos para que suene la alarma: "))
+total_seconds = minutes * 60 + seconds
+      
+alarm(total_seconds)
+
